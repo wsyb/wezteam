9354e9a8201b4722a319a4b382f9b44d91998648
wezteam <wezteam@users.noreply.github.com>
2026-07-06 18:20:21 +0800

    fix: use git tag name instead of datetime for releases

--- ci/tag-name.sh
+++ ci/tag-name.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y%m%d-%H%M%S"
+git describe --tags --exact-match 2>/dev/null || git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y%m%d-%H%M%S"
